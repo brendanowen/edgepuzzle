8f13134a9c2824d98778468e600a9bee1a500ca5
Brendan Owen <brendanowen@gmail.com>
2025-05-07 01:12:58 +0000

    Added the basic command line framework

---
+++ edge_cli/src/main.rs
@@ -0,0 +1,56 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(name = "Edge Puzzle CLI")]
+#[command(version = "1.0")]
+#[command(about = "Generates information about edge puzzles", long_about = None)]
+#[command(author = "Brendan Owen")]
+struct CliArgs {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Outputs known search profile
+    Profile {
+        /// Width of puzzle
+        #[arg(short, long, value_name = "INTEGER")]
+        x: usize,
+
+        /// Height of puzzle
+        #[arg(short, long, value_name = "INTEGER")]
+        y: usize,
+
+        /// Border edge types
+        #[arg(short, long, value_name = "INTEGER")]
+        border: usize,
+
+        /// Middle edge types
+        #[arg(short, long, value_name = "INTEGER")]
+        middle: usize,
+
+        /// Comma-delimited list of search orders to calculate.
+        #[arg(short, long, value_enum, num_args = 1.., value_delimiter = ',')]
+        searches: Vec<SearchOrder>, // Made optional to avoid requiring it
+
+        /// Output search profile. "-" writes to stdout.
+        #[arg(short, long, value_name = "CSV FILE")]
+        output: String,
+    },
+}
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum SearchOrder {
+    ScanRows,
+    ScanColumns,
+    ScanShortest,
+    SpiralIn,
+    SpiralOut,
+    FrameFirst,
+}
+
+fn main() {
+    // Parse the command-line arguments
+    let args = CliArgs::parse();
+}
---
+++ edgelib/src/lib.rs
@@ -0,0 +1,6 @@
+//! Polyform tools
+//!
+//! Provides a set of functions to enumerate and manipulate polyforms
+
+/// Key data structures to represent the polyforms
+pub mod model;
